f7d55e9885c94944dc0638ed39722d8f45cb7331
ar669 <83363575+ar669@users.noreply.github.com>
2025-12-01 16:34:47 +0000

    Add basic calculator functions
    Implemented basic calculator functions for addition, subtraction, multiplication, division, and exponentiation.

---
+++ calculator.py
@@ -0,0 +1,30 @@
+"""
+Basic Calculator for two numbers which can perform the following operations:
+addition, subtraction, multiplication and division
+"""
+
+def addition(num1, num2):
+
+    return num1 + num2
+
+def subtract(num1, num2):
+
+    return num1 - num2
+
+def multiply(num1, num2):
+
+    return num1 * num2
+
+def divide(num1, num2):
+
+    return num1 / num2
+
+#todo: implement exponent
+def exponent(num1, num2):
+    return num1 ** num2
+
+#todo: implement user input
+
+#todo: implement user interface
+
+print("Hello World")
